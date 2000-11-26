e3b7e3d795d7f53857ec05ef251131db5fa7d07a
Klaas Freitag <freitag@suse.de>
2000-11-26 14:05:10 +0000

    -whitespace changes :( -if-state added to avoid a division by zero.
    svn path=/trunk/kdegraphics/libkscan/; revision=72338

--- libkscan/kscan/kgammatable.cpp
+++ libkscan/kscan/kgammatable.cpp
@@ -5,48 +5,54 @@
 KGammaTable::KGammaTable( int gamma, int brightness, int contrast  ) 
     : QObject()
 {
-    g = gamma < 1 ? 1 : gamma;
-    b = brightness;
-    c = contrast;
-    gt.resize(256);
-    calcTable();
+   g = gamma < 1 ? 1 : gamma;
+   b = brightness;
+   c = contrast;
+   gt.resize(256);
+   calcTable();
 }
 
 const KGammaTable& KGammaTable::operator=(const KGammaTable& gt)
 {
-    if( this != &gt )
-    {
-	g = gt.g;
-	b = gt.b;
-	c = gt.c;
+   if( this != &gt )
+   {
+      g = gt.g;
+      b = gt.b;
+      c = gt.c;
 
-        calcTable();
-    }
+      calcTable();
+   }
 
-    return( *this );
+   return( *this );
 }
 
 inline SANE_Word adjust( SANE_Word x, int gl,int bl,int cl)
 {
-    //printf("x : %d, x/256 : %lg, g : %d, 100/g : %lg",
-    //       x,(double)x/256.0,g,100.0/(double)g);
-    x = ( SANE_Int )(256*pow((double)x/256.0,100.0/(double)gl));
-    x = ((65536/(128-cl)-256)*(x-128)>>8)+128+bl;
-    if(x<0) x = 0; else if(x>255) x = 255;
-    //printf(" -> %d\n",x);
-    return x;
+   //printf("x : %d, x/256 : %lg, g : %d, 100/g : %lg",
+   //       x,(double)x/256.0,g,100.0/(double)g);
+   x = ( SANE_Int )(256*pow((double)x/256.0,100.0/(double)gl));
+   x = ((65536/(128-cl)-256)*(x-128)>>8)+128+bl;
+   if(x<0) x = 0; else if(x>255) x = 255;
+   //printf(" -> %d\n",x);
+   return x;
 }
 
 void KGammaTable::calcTable( )
 {
-    int br = (b<<8)/(128-c);
-    int gr = g;
-    int cr = c;
+   int br = (b<<8)/(128-c);
+   int gr = g;
+   int cr = c;
 
-    for( SANE_Word i = 0; i<256; i++)
-	gt[i] = adjust(i, gr, br, cr );
+   if( gr == 0 )
+   {
+      debug( "Cant calc table -> would raise div. by zero !" );
+      return;
+   }
+   
+   for( SANE_Word i = 0; i<256; i++)
+      gt[i] = adjust(i, gr, br, cr );
 
-    dirty = false;
+   dirty = false;
 }
 
 void KGammaTable::setAll( int gamma, int brightness, int contrast )
