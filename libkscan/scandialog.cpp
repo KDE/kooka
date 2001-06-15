8f010fe90f4ca446547e661c155815b1042fe31d
Carsten Pfeiffer <pfeiffer@kde.org>
2001-06-15 02:11:19 +0000

    - abort the dialog, when the device-selection dialog was aborted   (update kio for that)
    svn path=/trunk/kdegraphics/libkscan/; revision=102245

--- libkscan/scandialog.cpp
+++ libkscan/scandialog.cpp
@@ -16,7 +16,7 @@
 
 extern "C" {
     void * init_libkscan() {
-	return new ScanDialogFactory;
+        return new ScanDialogFactory;
     }
 }
 
@@ -46,7 +46,7 @@ ScanDialog::ScanDialog( QWidget *parent, const char *name, bool modal )
     m_scanParams = new ScanParams( splitter );
     m_device = new KScanDevice( this );
     connect(m_device, SIGNAL(sigNewImage(QImage *)),
-	    this, SLOT(slotFinalImage(QImage *)));
+            this, SLOT(slotFinalImage(QImage *)));
 
     /* Create a preview widget to the right side of the splitter */
     m_previewer = new Previewer( splitter );
@@ -54,54 +54,49 @@ ScanDialog::ScanDialog( QWidget *parent, const char *name, bool modal )
     /* ... and connect to the selector-slots. They communicate user's
      * selection to the scanner parameter engine */
     connect( m_previewer->getImageCanvas(), SIGNAL( newRect(QRect)),
-	     m_scanParams, SLOT(slCustomScanSize(QRect)));
+             m_scanParams, SLOT(slCustomScanSize(QRect)));
     connect( m_previewer->getImageCanvas(), SIGNAL( noRect()),
-	     m_scanParams, SLOT(slMaximalScanSize()));	
-    
+             m_scanParams, SLOT(slMaximalScanSize()));	
+
     /* a new preview signal */
     connect( m_device, SIGNAL( sigNewPreview( QImage* )),
-	     this, SLOT( slotNewPreview( QImage* )));
-    
-    // QLabel *label = new QLabel( splitter );
-    // label->setBackgroundMode( PaletteBase );
+             this, SLOT( slotNewPreview( QImage* )));
 
     QStringList scannerNames;
     QStrList backends = m_device->getDevices();;
     QStrListIterator it( backends );
 
     while ( it.current() ) {
-	scannerNames.append( m_device->getScannerName( it.current() ));
-	++it;
+        scannerNames.append( m_device->getScannerName( it.current() ));
+        ++it;
     }
 
     resize(700, 500);
 
     if( scannerNames.count() > 0 )
     {
-       DeviceSelector ds( this, backends, scannerNames );
-       if ( ds.exec() == QDialog::Accepted )
-       {
-	  m_device->openDevice( ds.getSelectedDevice() );
+        DeviceSelector ds( this, backends, scannerNames );
+        if ( ds.exec() == QDialog::Accepted )
+        {
+            m_device->openDevice( ds.getSelectedDevice() );
 
-	  /* ConnectDevice does the gui for scanParams. */
-	  if ( !m_scanParams->connectDevice( m_device ) )
-	  {
-	     kdDebug(29000) << "ERR: Could not connect scan device" << endl;
-	  }	
-       }
-       else
-       {
-	  /** TODO: hmmm - this one should skip the entire action **/
-       }
+            /* ConnectDevice does the gui for scanParams. */
+            if ( !m_scanParams->connectDevice( m_device ) )
+            {
+                kdDebug(29000) << "ERR: Could not connect scan device" << endl;
+            }	
+        }
+        else // completely abort
+            setValid( false );
     }
     else
     {
-       /* No scanner found, open with information */
-       m_scanParams->connectDevice( 0L );
+        /* No scanner found, open with information */
+        m_scanParams->connectDevice( 0L );
 
-       /* Making the previewer gray */
-       /* disabling is much better than hiding for 'advertising' ;) */
-       m_previewer->setEnabled( false );
+        /* Making the previewer gray */
+        /* disabling is much better than hiding for 'advertising' ;) */
+        m_previewer->setEnabled( false );
     }
 }
 
@@ -109,16 +104,16 @@ ScanDialog::ScanDialog( QWidget *parent, const char *name, bool modal )
 
 void ScanDialog::slotNewPreview( QImage *image )
 {
-   if( image )
-   {
-      m_previewImage = *image;
-      // hmmm - dont know, if conversion of the bit-depth is neccessary.
-      // m_previewImage.convertDepth(32);
+    if( image )
+    {
+        m_previewImage = *image;
+        // hmmm - dont know, if conversion of the bit-depth is neccessary.
+        // m_previewImage.convertDepth(32);
 
-      /* The previewer does not copy the image data ! */
-      m_previewer->newImage( &m_previewImage );
-   }
-   
+        /* The previewer does not copy the image data ! */
+        m_previewer->newImage( &m_previewImage );
+    }
+
 }
 
 ScanDialog::~ScanDialog()
