b176f8d636c10bd3e4e56c61f0ef1dcba79b08e4
Klaas Freitag <freitag@suse.de>
2001-07-13 14:54:06 +0000

    - added keyboard shortcuts - added a options tab to fix the bug that you can select NOT to see the   device selector on startup, but never can reinvent to see it again.   The option tab allows to toggle the appearance of the scanner selector   on startup and if network should be queried for network scanners (which   must be configured in sane's network configuration file, this functionality   does not spam the entire network ;-) - added private d pointers.
    svn path=/trunk/kdegraphics/libkscan/; revision=105935

--- libkscan/scandialog.cpp
+++ libkscan/scandialog.cpp
@@ -2,10 +2,14 @@
 #include <qlayout.h>
 #include <qstringlist.h>
 #include <qstrlist.h>
+#include <qtooltip.h>
+#include <qsizepolicy.h> 
 
 #include <kglobal.h>
 #include <klocale.h>
 #include <kdebug.h>
+#include <kconfig.h>
+#include <ksimpleconfig.h>
 
 // libkscan stuff
 #include "scanparams.h"
@@ -40,7 +44,8 @@ KScanDialog * ScanDialogFactory::createDialog( QWidget *parent,
 ScanDialog::ScanDialog( QWidget *parent, const char *name, bool modal )
     : KScanDialog( Tabbed, Close|Help, parent, name, modal )
 {
-    QVBox *page = addVBoxPage( i18n("Scanning") );
+    QVBox *page = addVBoxPage( i18n("&Scanning") );
+    
     QSplitter *splitter = new QSplitter( Horizontal, page, "splitter" );
 
     m_scanParams = new ScanParams( splitter );
@@ -121,8 +126,77 @@ ScanDialog::ScanDialog( QWidget *parent, const char *name, bool modal )
     }
     
     m_previewer->setEnabled( false );
+
+    /* Options-page */
+    createOptionsTab( );
 }
 
+
+void ScanDialog::createOptionsTab( void )
+{
+
+   QVBox *page = addVBoxPage( i18n("&Options"));
+   setMainWidget(page);	   
+
+   QGroupBox *gb = new QGroupBox( 1, Qt::Horizontal, i18n("Startup options"), page, "GB_STARTUP" );
+   QLabel *label = new QLabel( i18n( "Note: changing this options will affect the scan plugin on next start." ),
+			       gb );
+   label->setSizePolicy( QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed ) );
+   
+   /* Checkbox for asking for scanner on startup */
+   cb_askOnStart = new QCheckBox( i18n( "&Ask for the scan device on plugin startup"), gb );
+   QToolTip::add( cb_askOnStart,
+		  i18n("You can uncheck this if you do not want to be asked which scanner to use on startup."));
+   CHECK_PTR( cb_askOnStart );
+
+   /* Checkbox for network access */
+   cb_network = new QCheckBox( i18n( "&Query the network for scan devices"), gb );
+   QToolTip::add( cb_network,
+		  i18n("Check this if you want to query for configured network scan stations."));
+   CHECK_PTR( cb_network );
+
+
+   /* Read settings for startup behavior */
+   KConfig *gcfg = KGlobal::config(); 
+   gcfg->setGroup(QString::fromLatin1(GROUP_STARTUP));
+   bool skipDialog  = gcfg->readBoolEntry( STARTUP_SKIP_ASK, false );	
+   bool onlyLocal   = gcfg->readBoolEntry( STARTUP_ONLY_LOCAL, false );
+   
+   /* Note: flag must be inverted because of question is 'the other way round' */
+   cb_askOnStart->setChecked( !skipDialog );
+   connect( cb_askOnStart, SIGNAL(toggled(bool)), this, SLOT(slotAskOnStartToggle(bool)));
+
+   cb_network->setChecked( !onlyLocal );
+   connect( cb_network, SIGNAL(toggled(bool)), this, SLOT(slotNetworkToggle(bool)));
+   
+   
+   QWidget *spaceEater = new QWidget( page );
+   CHECK_PTR( spaceEater );
+   spaceEater->setSizePolicy( QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding) );
+
+}
+
+void ScanDialog::slotNetworkToggle( bool state)
+{
+   bool writestate = !state;
+
+   kdDebug(29000) << "slotNetworkToggle: Writing state " << writestate << endl;
+   KSimpleConfig *c = new KSimpleConfig(QString::fromLatin1("kdeglobals"), false);
+   c->setGroup(QString::fromLatin1(GROUP_STARTUP));
+   c->writeEntry( STARTUP_ONLY_LOCAL, writestate );
+   delete c;
+}
+
+void ScanDialog::slotAskOnStartToggle(bool state)
+{
+   bool writestate = !state;
+   
+   kdDebug(29000) << "slotAskOnStartToggle: Writing state " << writestate << endl;
+   KSimpleConfig *c = new KSimpleConfig(QString::fromLatin1("kdeglobals"), false);
+   c->setGroup(QString::fromLatin1(GROUP_STARTUP));
+   c->writeEntry( STARTUP_SKIP_ASK, writestate );
+   delete c;
+}
 
 bool ScanDialog::setup()
 {
--- libkscan/scandialog.h
+++ libkscan/scandialog.h
@@ -11,33 +11,42 @@ class Previewer;
 
 class ScanDialog : public KScanDialog
 {
-    Q_OBJECT
+   Q_OBJECT
 
 public:
-    ScanDialog( QWidget *parent=0, const char *name=0, bool modal=false );
-    ~ScanDialog();
+   ScanDialog( QWidget *parent=0, const char *name=0, bool modal=false );
+   ~ScanDialog();
 
-    virtual bool setup();
-    
-private slots:
-    void slotFinalImage(QImage *);
-   void slotNewPreview( QImage *);
+   virtual bool setup();
+
 private:
-    ScanParams  *m_scanParams;
-    KScanDevice *m_device;
-    Previewer    *m_previewer;
-    QImage m_previewImage;
-   bool good_scan_connect;
+   void createOptionsTab( void );
+      
+private slots:
+   void slotFinalImage(QImage *);
+   void slotNewPreview( QImage *);
+   void slotAskOnStartToggle(bool state);
+   void slotNetworkToggle( bool state);
+private:
+   ScanParams   *m_scanParams;
+   KScanDevice  *m_device;
+   Previewer    *m_previewer;
+   QImage       m_previewImage;
+   bool         good_scan_connect;
+   QCheckBox    *cb_askOnStart;
+   QCheckBox    *cb_network;
+   class ScanDialogPrivate;
+   ScanDialogPrivate *d;
 };
 
 class ScanDialogFactory : public KScanDialogFactory
 {
 public:
-    ScanDialogFactory( QObject *parent=0, const char *name=0 );
+   ScanDialogFactory( QObject *parent=0, const char *name=0 );
 
 protected:
-    virtual KScanDialog * createDialog( QWidget *parent=0, const char *name=0,
-					bool modal=false );
+   virtual KScanDialog * createDialog( QWidget *parent=0, const char *name=0,
+				       bool modal=false );
 
 
 };
