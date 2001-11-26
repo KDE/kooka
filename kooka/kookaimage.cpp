ca9d1443fb7d365da461b1c17b7c183e22659d5f
Klaas Freitag <freitag@suse.de>
2001-11-26 09:58:38 +0000

    Kooka image object
    svn path=/trunk/kdegraphics/kooka/; revision=124256

---
+++ kooka/kookaimage.cpp
@@ -0,0 +1,37 @@
+/***************************************************************************
+                          kookaimage.cpp  - Kooka's Image 
+                             -------------------                                         
+    begin                : Thu Nov 20 2001
+    copyright            : (C) 1999 by Klaas Freitag                         
+    email                : freitag@suse.de                                     
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   * 
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "kookaimage.h"
+
+/**
+  *@author Klaas Freitag
+  */
+
+
+KookaImage::KookaImage( )
+   : QImage()
+{
+   
+}
+
+KookaImage::~KookaImage()
+{
+   
+}
+
+
---
+++ kooka/kookaimage.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+                          kookaimage.h  - Kooka's Image 
+                             -------------------                                         
+    begin                : Thu Nov 20 2001
+    copyright            : (C) 1999 by Klaas Freitag                         
+    email                : freitag@suse.de                                     
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   * 
+ *                                                                         *
+ ***************************************************************************/
+
+
+#ifndef KOOKAIMAGE_H
+#define KOOKAIMAGE_H
+
+#include <qimage.h>
+
+/**
+  *@author Klaas Freitag
+  */
+
+
+class KookaImage: public QImage
+{
+public:
+   KookaImage( );
+
+   ~KookaImage();
+
+   
+};
+
+#endif
