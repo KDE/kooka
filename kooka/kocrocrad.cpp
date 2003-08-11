03174c7b0beb7e383dd26de97fec1c82520c81a9
Klaas Freitag <freitag@suse.de>
2003-08-11 12:10:21 +0000

    added ocrad gui for OCR
    svn path=/trunk/kdegraphics/kooka/; revision=242067

---
+++ kooka/kocrocrad.cpp
@@ -0,0 +1,179 @@
+/***************************************************************************
+                          kocrocrad.cpp  - ocrad dialog
+                             -------------------
+    begin                : Tue Jul 15 2003
+    copyright            : (C) 2003 by Klaas Freitag
+    email                : freitag@suse.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This file may be distributed and/or modified under the terms of the    *
+ *  GNU General Public License version 2 as published by the Free Software *
+ *  Foundation and appearing in the file COPYING included in the           *
+ *  packaging of this file.                                                *
+ *
+ *  As a special exception, permission is given to link this program       *
+ *  with any version of the KADMOS ocr/icr engine of reRecognition GmbH,   *
+ *  Kreuzlingen and distribute the resulting executable without            *
+ *  including the source code for KADMOS in the source distribution.       *
+ *
+ *  As a special exception, permission is given to link this program       *
+ *  with any edition of Qt, and distribute the resulting executable,       *
+ *  without including the source code for Qt in the source distribution.   *
+ *                                                                         *
+ ***************************************************************************/
+
+/* $Id$ */
+
+#include <qlayout.h>
+#include <qlabel.h>
+#include <qfileinfo.h>
+#include <qtooltip.h>
+
+#include <kapplication.h>
+#include <kconfig.h>
+#include <kglobal.h>
+#include <kdebug.h>
+#include <klocale.h>
+#include <kanimwidget.h>
+#include <kseparator.h>
+#include <kmessagebox.h>
+#include <kurlrequester.h>
+
+#include "resource.h"
+#include "kocrocrad.h"
+#include <kscanslider.h>
+#include "kookaimage.h"
+#include "kookapref.h"
+#include <qvbox.h>
+#include <qhbox.h>
+
+
+ocradDialog::ocradDialog( QWidget *parent, KSpellConfig *spellConfig )
+    :KOCRBase( parent, spellConfig, KDialogBase::Tabbed ),
+     m_ocrCmd( QString()),
+     m_orfUrlRequester(0L)
+{
+   kdDebug(28000) << "Starting ocrad-Start-Dialog!" << endl;
+   // Layout-Boxes
+}
+
+QString ocradDialog::ocrEngineLogo() const
+{
+    return "";  // TODO
+}
+
+QString ocradDialog::ocrEngineName() const
+{
+    return i18n("ocrad" );
+}
+
+QString ocradDialog::ocrEngineDesc() const
+{
+    return i18n("ocrad is an Open Source project "
+                "for optical character recognition.<p>"
+                "The author of gocr is <b>Antonio Diaz</b><br>"
+                "For more information about ocrad see "
+                "<A HREF=\"http://savannah.gnu.org/projects/ocrad\">"
+                "http://savannah.gnu.org/projects/ocrad</A><p>"
+        "Images should be scanned in black/white mode for ocrad.<br>"
+        "Best results are achieved if the characters are at least 20 pixels high.<p>"
+        "Problems arise, as usual, with very bold or very light or broken characters, "
+        "the same with merged character groups.");
+}
+
+EngineError ocradDialog::setupGui()
+{
+    KOCRBase::setupGui();
+
+    QVBox *page = ocrPage();
+    Q_CHECK_PTR( page );
+
+    KConfig *conf = KGlobal::config ();
+    conf->setGroup( CFG_GROUP_OCR_DIA );
+
+    // Horizontal line
+    // (void) new  KSeparator( KSeparator::HLine, page);
+
+    // Entry-Field.
+    QString res = conf->readPathEntry( CFG_OCRAD_BINARY, "notFound" );
+    if( res == "notFound" )
+    {
+        res = KookaPreferences::tryFindBinary("ocrad", CFG_OCRAD_BINARY);
+        if( res.isEmpty() )
+        {
+            /* Popup here telling that the config needs to be called */
+            KMessageBox::sorry( this, i18n( "The path to the ocrad binary is not configured yet.\n"
+                                            "Please go to the Kooka configuration and enter the path manually."),
+                                i18n("OCR Software not Found") );
+        }
+    }
+
+    if( res.isEmpty() )
+        res = i18n("Not found");
+    else
+        m_ocrCmd = res;
+
+    (void) new KSeparator( KSeparator::HLine, page);
+    QHBox *hb = new QHBox(page);
+    hb->setSpacing( KDialog::spacingHint());
+    (void) new QLabel( i18n("Using ocrad binary: ") + res, hb );
+    getAnimation(hb);
+
+#if 0
+    QVBox *innerBox = new QVBox( hb );
+    innerBox->setSpacing( KDialog::spacingHint());
+    QHBox *hbox = new QHBox( innerBox );
+
+    (void) new QLabel( i18n("Select an orf:"), hbox );
+    m_orfUrlRequester = new KURLRequester( hbox );
+    m_orfUrlRequester->setMode( KFile::File | KFile::ExistingOnly | KFile::LocalOnly );
+
+    QToolTip::add( m_orfUrlRequester,
+                   i18n( "Select an OCR Result File to analyse" ));
+#endif
+    /* This is for a 'work-in-progress'-Animation */
+
+    return ENG_OK;
+}
+
+void ocradDialog::introduceImage( KookaImage *img )
+{
+    if( !img ) return;
+
+    KOCRBase::introduceImage( img );
+}
+
+
+ocradDialog::~ocradDialog()
+{
+
+}
+
+void ocradDialog::writeConfig( void )
+{
+   KConfig *conf = KGlobal::config ();
+   conf->setGroup( CFG_GROUP_OCR_DIA );
+
+   conf->writeEntry( CFG_OCRAD_BINARY, QString(getOCRCmd()));
+}
+
+
+void ocradDialog::enableFields(bool )
+{
+    kdDebug(28000) << "About to disable the entry fields" << endl;
+}
+
+QString ocradDialog::orfUrl() const
+{
+    if( m_orfUrlRequester )
+	return m_orfUrlRequester->url();
+    else
+	return QString();
+}
+
+#include "kocrocrad.moc"
+
+/* The End ;) */
+
---
+++ kooka/kocrocrad.h
@@ -0,0 +1,76 @@
+/***************************************************************************
+                     kocrocrad.h - ocr dialog for ocrad
+                             -------------------
+    begin                : Tue Jul 15 2003
+    copyright            : (C) 2003 by Klaas Freitag
+    email                : freitag@suse.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This file may be distributed and/or modified under the terms of the    *
+ *  GNU General Public License version 2 as published by the Free Software *
+ *  Foundation and appearing in the file COPYING included in the           *
+ *  packaging of this file.                                                *
+ *
+ *  As a special exception, permission is given to link this program       *
+ *  with any version of the KADMOS ocr/icr engine of reRecognition GmbH,   *
+ *  Kreuzlingen and distribute the resulting executable without            *
+ *  including the source code for KADMOS in the source distribution.       *
+ *
+ *  As a special exception, permission is given to link this program       *
+ *  with any edition of Qt, and distribute the resulting executable,       *
+ *  without including the source code for Qt in the source distribution.   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#ifndef KOCROCRAD_H
+#define KOCROCRAD_H
+
+#include <kdialogbase.h>
+#include <qimage.h>
+#include <qstring.h>
+
+#include <kscanslider.h>
+#include <kanimwidget.h>
+
+#include "kocrbase.h"
+/**
+  *@author Klaas Freitag
+  */
+
+class KSpellConfig;
+class KURLRequester;
+
+class ocradDialog: public KOCRBase
+{
+    Q_OBJECT
+public:
+    ocradDialog( QWidget*, KSpellConfig* );
+    ~ocradDialog();
+
+    QString getOCRCmd( void ) const
+        { return m_ocrCmd;}
+
+    EngineError setupGui();
+
+    QString ocrEngineName() const;
+    QString ocrEngineDesc() const;
+    QString ocrEngineLogo() const;
+
+    QString orfUrl() const;
+public slots:
+    void enableFields(bool);
+    void introduceImage( KookaImage* );
+
+protected:
+    void writeConfig();
+
+
+private:
+    QString      m_ocrCmd;
+    KURLRequester *m_orfUrlRequester;
+};
+
+#endif
