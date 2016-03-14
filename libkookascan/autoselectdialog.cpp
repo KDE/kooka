1fc7fceac9212e8efa3dcc01c6fdb64af9cf5d0b
Jonathan Marten <jjm@keelhaul.me.uk>
2016-03-14 17:03:03 +0000

    Port the "Autoselect Settings" dialogue

--- libkookascan/autoselectdialog.cpp
+++ libkookascan/autoselectdialog.cpp
@@ -1,26 +1,31 @@
 /************************************************************************
- *                                  *
- *  This file is part of libkscan, a KDE scanning library.      *
- *                                  *
- *  Copyright (C) 2013 Jonathan Marten <jjm@keelhaul.me.uk>     *
- *                                  *
- *  This library is free software; you can redistribute it and/or   *
- *  modify it under the terms of the GNU Library General Public     *
- *  License as published by the Free Software Foundation and appearing  *
- *  in the file COPYING included in the packaging of this file;     *
- *  either version 2 of the License, or (at your option) any later  *
- *  version.                                *
- *                                  *
- *  This program is distributed in the hope that it will be useful, *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   *
- *  GNU General Public License for more details.            *
- *                                  *
- *  You should have received a copy of the GNU General Public License   *
- *  along with this program;  see the file COPYING.  If not, write to   *
- *  the Free Software Foundation, Inc., 51 Franklin Street,     *
- *  Fifth Floor, Boston, MA 02110-1301, USA.                *
- *                                  *
+ *									*
+ *  This file is part of Kooka, a scanning/OCR application using	*
+ *  Qt <http://www.qt.io> and KDE Frameworks <http://www.kde.org>.	*
+ *									*
+ *  Copyright (C) 2013-2016 Jonathan Marten <jjm@keelhaul.me.uk>	*
+ *									*
+ *  Kooka is free software; you can redistribute it and/or modify it	*
+ *  under the terms of the GNU Library General Public License as	*
+ *  published by the Free Software Foundation and appearing in the	*
+ *  file COPYING included in the packaging of this file;  either	*
+ *  version 2 of the License, or (at your option) any later version.	*
+ *									*
+ *  As a special exception, permission is given to link this program	*
+ *  with any version of the KADMOS OCR/ICR engine (a product of		*
+ *  reRecognition GmbH, Kreuzlingen), and distribute the resulting	*
+ *  executable without including the source code for KADMOS in the	*
+ *  source distribution.						*
+ *									*
+ *  This program is distributed in the hope that it will be useful,	*
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of	*
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	*
+ *  GNU General Public License for more details.			*
+ *									*
+ *  You should have received a copy of the GNU General Public		*
+ *  License along with this program;  see the file COPYING.  If		*
+ *  not, see <http://www.gnu.org/licenses/>.				*
+ *									*
  ************************************************************************/
 
 #include "autoselectdialog.h"
@@ -28,14 +33,10 @@
 #include <qcombobox.h>
 #include <qformlayout.h>
 #include <qlabel.h>
-#include <qslider.h>
+#include <qdebug.h>
+#include <qpushbutton.h>
 
-#include <KLocalizedString>
-#include <QDebug>
-#include <KConfigGroup>
-#include <QDialogButtonBox>
-#include <QPushButton>
-#include <QVBoxLayout>
+#include <klocalizedstring.h>
 
 #include "kscancontrols.h"
 #include "kscandevice.h"
@@ -46,27 +47,23 @@
 #define INDEX_BLACK		0
 #define INDEX_WHITE		1
 
+
 AutoSelectDialog::AutoSelectDialog(QWidget *parent)
-    : QDialog(parent)
+    : DialogBase(parent)
 {
-    //qDebug();
     setObjectName("AutoSelectDialog");
 
+    setModal(true);
     setWindowTitle(i18nc("@title:window", "Autoselect Settings"));
-    mButtonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel | QDialogButtonBox::Apply);
-    QVBoxLayout *mainLayout = new QVBoxLayout;
-    setLayout(mainLayout);
-    QPushButton *okButton = mButtonBox->button(QDialogButtonBox::Ok);
-    okButton->setDefault(true);
-    okButton->setShortcut(Qt::CTRL | Qt::Key_Return);
-    connect(mButtonBox, SIGNAL(accepted()), this, SLOT(accept()));
-    connect(mButtonBox, SIGNAL(rejected()), this, SLOT(reject()));
+    setButtons(QDialogButtonBox::Ok|QDialogButtonBox::Cancel|QDialogButtonBox::Apply);
 
-    QFormLayout *fl = new QFormLayout;          // looks better with combo expanded
+    QWidget *w = new QWidget(this);
+    QFormLayout *fl = new QFormLayout(w);		// looks better with combo expanded
     fl->setFieldGrowthPolicy(QFormLayout::AllNonFixedFieldsGrow);
 
     // slider for add/subtract margin
     const KConfigSkeletonItem *item = ScanSettings::self()->previewAutoselMarginItem();
+    Q_ASSERT(item!=NULL);
     int defaultVal = KScanDevice::getDefault<int>(item);
     int maxVal = item->maxValue().toInt();
     int minVal = item->minValue().toInt();
@@ -77,10 +74,11 @@ AutoSelectDialog::AutoSelectDialog(QWidget *parent)
     connect(mMarginSlider, SIGNAL(settingChanged(int)), SLOT(slotControlChanged()));
     fl->addRow(item->label(), mMarginSlider);
 
-//TODO PORT QT5     fl->addItem(new QSpacerItem(1, QDialog::marginHint()));
+    fl->addItem(new QSpacerItem(1, verticalSpacing()));
 
     // combobox to select whether black or white background
     item = ScanSettings::self()->previewAutoselBackgroundItem();
+    Q_ASSERT(item!=NULL);
     mBackgroundCombo = new QComboBox;
     mBackgroundCombo->insertItem(INDEX_BLACK, i18n("Black"));
     mBackgroundCombo->insertItem(INDEX_WHITE, i18n("White"));
@@ -90,6 +88,7 @@ AutoSelectDialog::AutoSelectDialog(QWidget *parent)
 
     // slider for dust size - apparently not really much impact on the result
     item = ScanSettings::self()->previewAutoselDustsizeItem();
+    Q_ASSERT(item!=NULL);
     defaultVal = KScanDevice::getDefault<int>(item);
     maxVal = item->maxValue().toInt();
     minVal = item->minValue().toInt();
@@ -100,24 +99,16 @@ AutoSelectDialog::AutoSelectDialog(QWidget *parent)
     connect(mDustsizeSlider, SIGNAL(settingChanged(int)), SLOT(slotControlChanged()));
     fl->addRow(item->label(), mDustsizeSlider);
 
-    QWidget *w = new QWidget;
-    w->setLayout(fl);
-    mainLayout->addWidget(w);
-    mainLayout->addWidget(mButtonBox);
+    setMainWidget(w);
 
-    connect(mButtonBox->button(QDialogButtonBox::Apply), SIGNAL(clicked()), SLOT(slotApplySettings()));
-    connect(okButton, SIGNAL(clicked()), SLOT(slotApplySettings()));
-    connect(okButton, SIGNAL(clicked()), SLOT(accept()));
-    mButtonBox->button(QDialogButtonBox::Apply)->setEnabled(false);
-}
-
-AutoSelectDialog::~AutoSelectDialog()
-{
+    connect(buttonBox()->button(QDialogButtonBox::Apply), SIGNAL(clicked()), SLOT(slotApplySettings()));
+    connect(buttonBox()->button(QDialogButtonBox::Ok), SIGNAL(clicked()), SLOT(slotApplySettings()));
+    buttonBox()->button(QDialogButtonBox::Apply)->setEnabled(false);
 }
 
 void AutoSelectDialog::slotControlChanged()
 {
-    mButtonBox->button(QDialogButtonBox::Apply)->setEnabled(true);
+    buttonBox()->button(QDialogButtonBox::Apply)->setEnabled(true);
 }
 
 void AutoSelectDialog::slotApplySettings()
@@ -126,7 +117,7 @@ void AutoSelectDialog::slotApplySettings()
     const bool bgIsWhite = (mBackgroundCombo->currentIndex()==INDEX_WHITE);
     const int dustsize = mDustsizeSlider->value();
     emit settingsChanged(margin, bgIsWhite, dustsize);
-    mButtonBox->button(QDialogButtonBox::Apply)->setEnabled(false);
+    buttonBox()->button(QDialogButtonBox::Apply)->setEnabled(false);
 }
 
 void AutoSelectDialog::setSettings(int margin, bool bgIsWhite, int dustsize)
@@ -134,4 +125,5 @@ void AutoSelectDialog::setSettings(int margin, bool bgIsWhite, int dustsize)
     mMarginSlider->setValue(margin);
     mBackgroundCombo->setCurrentIndex(bgIsWhite ? INDEX_WHITE : INDEX_BLACK);
     mDustsizeSlider->setValue(dustsize);
+    buttonBox()->button(QDialogButtonBox::Apply)->setEnabled(false);
 }
