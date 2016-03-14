624bd574a005c1b7833989ed3fa3cbef30121830
Jonathan Marten <jjm@keelhaul.me.uk>
2016-03-14 13:52:17 +0000

    Port main() to KAboutData and QCommandLineParser

--- kooka/main.cpp
+++ kooka/main.cpp
@@ -31,97 +31,88 @@
 
 #include <qdebug.h>
 #include <qapplication.h>
+#include <qcommandlineparser.h>
 
-#include <kapplication.h>
-#include <K4AboutData>
-#include <kcmdlineargs.h>
+#include <kaboutdata.h>
 #include <klocalizedstring.h>
-#include <kglobal.h>
 
 #include "kooka.h"
 #include "vcsversion.h"
 
 
-static const char shortDesc[] = I18N_NOOP("Scanning, image gallery and OCR");
+static const char shortDesc[] =
+    I18N_NOOP("Scanning, image gallery and OCR");
 
-static const char longDesc[] = I18N_NOOP("Kooka provides access to scanner hardware using the "
-                               "<a href=\"http://www.sane-project.org/\">SANE</a> library."
-                               "\n\n"
-                               "Kooka allows you to scan, save and view in any image format that KDE supports, "
-                               "and can perform Optical Character Recognition using the open source "
-                               "<a href=\"http://jocr.sourceforge.net/\">GOCR</a> or "
-                               "<a href=\"http://www.gnu.org/software/ocrad/ocrad.html\">OCRAD</a> programs, "
-                               "or the commercial <a href=\"http://www.rerecognition.com/\">KADMOS</a> library.");
+static const char longDesc[] =
+    I18N_NOOP("Kooka provides access to scanner hardware using the "
+              "<a href=\"http://www.sane-project.org/\">SANE</a> library."
+              "\n\n"
+              "Kooka allows you to scan, save and view in any image format that KDE supports, "
+              "and can perform Optical Character Recognition using the open source "
+              "<a href=\"http://jocr.sourceforge.net/\">GOCR</a> or "
+              "<a href=\"http://www.gnu.org/software/ocrad/ocrad.html\">OCRAD</a> programs, "
+              "or the commercial <a href=\"http://www.rerecognition.com/\">KADMOS</a> library.");
 
-// TODO: is the second paragraph of this licence needed with GPL Qt?
 static const char addLicense[] =
-    "This program is distributed under the terms of the GPL v2 as published\n"
-    "by the Free Software Foundation.\n"
-    "\n"
-    "As a special exception, permission is given to link this program\n"
-    "with any edition of Qt, and distribute the resulting executable\n"
-    "without including the source code for Qt in the source distribution.\n"
-    "\n"
-    "As a special exception, permission is given to link this program\n"
-    "with any version of the KADMOS OCR/ICR engine of reRecognition GmbH\n"
-    "(http://www.rerecognition.com), and distribute the resulting executable\n"
-    "without including the source code for KADMOS in the source distribution.\n"
-    "\n"
-    "Note that linking against KADMOS is not permitted under the terms of\n"
-    "the Qt GPL licence, so if you wish to do this then you must have a\n"
-    "commercial Qt development licence.\n";
+    I18N_NOOP("This program is distributed under the terms of the GPL v2 as published\n"
+              "by the Free Software Foundation.\n"
+              "\n"
+              "As a special exception, permission is given to link this program\n"
+              "with any version of the KADMOS OCR/ICR engine of reRecognition GmbH\n"
+              "(http://www.rerecognition.com), and distribute the resulting executable\n"
+              "without including the source code for KADMOS in the source distribution.\n"
+              "\n"
+              "Note that linking against KADMOS is not permitted under the terms of\n"
+              "the Qt GPL licence, so if you wish to do this then you must have a\n"
+              "commercial Qt development licence.\n");
 
 static const char copyright[] =
-    I18N_NOOP("(C) 2000-2014, the Kooka developers and contributors");
+    I18N_NOOP("(C) 2000-2016, the Kooka developers and contributors");
 
 
 int main(int argc, char *argv[])
 {
-    K4AboutData about("kooka",              // appName
-                      "",                // catalogName
-                      ki18n("Kooka"),            // programName
+    QApplication app(argc, argv);			// first of all, so that i18n() works
+
+    KAboutData about("kooka",					// componentName
+                     i18n("Kooka"),				// displayName
 #if VCS_AVAILABLE
-                      (VERSION " (" VCS_TYPE " " VCS_REVISION ")"),
+                     (VERSION " (" VCS_TYPE " " VCS_REVISION ")"),
 #else
-//QT5 fix me
-                      "VERSION",             // version
+                     "VERSION",				// version
 #endif
-                      ki18n(shortDesc),          // shortDescription
-                      K4AboutData::License_GPL_V2,   // licenseType
-                      ki18n(copyright),          // copyrightStatement
-                      ki18n(longDesc),           // text
-                      "http://techbase.kde.org/Projects/Kooka");
+                     i18n(shortDesc),				// shortDescription
+                     KAboutLicense::GPL_V2,			// licenseType
+                     i18n(copyright),				// copyrightStatement
+                     i18n(longDesc),				// otherText
+                     "http://techbase.kde.org/Projects/Kooka");	// homePageAddress
 
-    about.addAuthor(ki18n("Jonathan Marten"), ki18n("Current maintainer, KDE4 port"), "jjm@keelhaul.me.uk");
-    about.addAuthor(ki18n("Klaas Freitag"), ki18n("Developer"), "freitag@suse.de");
-    about.addCredit(ki18n("Mat Colton"), ki18n("Graphics, web"), "mat@colton.de");
-    about.addCredit(ki18n("Ivan Shvedunov"), ki18n("Original kscan application"), "ivan@rf-hp.npi.msu.su");
-    about.addCredit(ki18n("Alex Kempshall"), ki18n("Photocopy facility"), "alexkempshall@btinternet.com");
-    about.addLicenseText(ki18n(addLicense));
+    about.addAuthor(i18n("Jonathan Marten"), i18n("Current maintainer, KDE4 port"), "jjm@keelhaul.me.uk");
+    about.addAuthor(i18n("Montel Laurent"), i18n("Initial KF5 port"), "montel@kde.org");
+    about.addAuthor(i18n("Klaas Freitag"), i18n("Developer"), "freitag@suse.de");
+    about.addCredit(i18n("Mat Colton"), i18n("Graphics, web"), "mat@colton.de");
+    about.addCredit(i18n("Ivan Shvedunov"), i18n("Original kscan application"), "ivan@rf-hp.npi.msu.su");
+    about.addCredit(i18n("Alex Kempshall"), i18n("Photocopy facility"), "alexkempshall@btinternet.com");
+    about.addLicenseText(i18n(addLicense));
 
-    KCmdLineArgs::init(argc, argv, &about);
+    KAboutData::setApplicationData(about);
 
-    KCmdLineOptions options;
-    options.add("d <device>", ki18n("The SANE device specification (e.g. 'umax:/dev/sg0')"));
-    options.add("g", ki18n("Gallery mode - do not connect to scanner"));
-    KCmdLineArgs::addCmdLineOptions(options);       // Add my own options
+    QCommandLineParser parser;
+    parser.setApplicationDescription(about.shortDescription());
+    parser.addHelpOption();
+    parser.addVersionOption();
 
-    KApplication app;
-    QCoreApplication::setApplicationName(about.appName());
+    QCommandLineOption opt = QCommandLineOption("g", i18n("Gallery mode - do not connect to scanner"));
+    parser.addOption(opt);
 
-    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
-    QString devToUse = args->getOption("d");
-    if (args->isSet("g")) {
-        devToUse = "gallery";
-    }
+    opt = QCommandLineOption("d", i18n("The SANE device specification (e.g. 'umax:/dev/sg0')"), i18n("device"));
+    parser.addOption(opt);
+
+    parser.process(app);
+
+    QString devToUse = parser.value("d");
+    if (parser.isSet("g")) devToUse = "gallery";
     //qDebug() << "DevToUse is" << devToUse;
-
-// TODO: not sure what this did
-//    if (args->count()==1)
-//    {
-//        args->usage();
-//        // exit(-1);
-//    }
 
     // TODO: try ScanGlobal::init(), if that fails no point in carrying on
     // so show an error box and give up (or can we carry on and run in
@@ -131,7 +122,6 @@ int main(int argc, char *argv[])
     kooka->show();
     app.processEvents();
     kooka->startup();
-    args->clear();
 
     return (app.exec());
 }
