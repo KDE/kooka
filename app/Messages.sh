38f900e4037126dd3f1b217c9771a8eac4c6af9e
Jonathan Marten <jjm@keelhaul.me.uk>
2019-11-04 10:20:35 +0000

    Extract I18N messages for plugins (into 'kooka.po')

--- app/Messages.sh
+++ app/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/kooka.pot
+$XGETTEXT `find . -name \*.cpp` `find ../plugins -name \*.cpp` -o $podir/kooka.pot
 rm -f rc.cpp
